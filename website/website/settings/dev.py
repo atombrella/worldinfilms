0567f66d32fbd881eeb87ab00c381f6ea62811ea
Mads Jensen <mje@inducks.org>
2018-01-19 13:43:53 +0100

    Base Wagtail installation

---
+++ website/website/settings/dev.py
@@ -0,0 +1,16 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = os.getenv('WORLDINFILMS_SECRET_KEY', 'not-a-secret')
+
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
